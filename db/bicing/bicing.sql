3f5e69ddae8fb14277d2e01969fd2b7702454a65
mateu85 <mateupego85@hotmail.com>
2020-02-07 17:26:25 +0100

    06/02/2020 17:26

--- db/bicing/bicing.sql
+++ db/bicing/bicing.sql
@@ -14,27 +14,51 @@ fecha_alta datetime default current_timestamp
 );
 insert into usuarios values
 ("53118274P","Albert","Valles","albert@gmail.com",default),
-("93318174B","Lluis","Llorens","llorens@gmail.com",default);
+("93318174B","Lluis","Llorens","llorens@gmail.com",default),
+("13315174E","Jordi","Esteve","loreto@gmail.com",default),
+("53315174C","Carlos","Randero","randero@gmail.com",default),
+("73315174Y","Salva","Bolufer","bolufer@gmail.com",default),
+("84445174W","Joaquim","Pietro","pietro@gmail.com",default),
+("33315174D","Joan","Valero","valero@gmail.com",default),
+("52215174T","Charly","Vinent","vicents@gmail.com",default),
+("21115174V","Andreu","Casals","casals@gmail.com",default),
+("63315174C","Lluis","Bertomeu","bertomeu@gmail.com",default),
+("83225174A","Alvaro","Laporta","laporta@gmail.com",default);
 
+create table estaciones(
+id_est int primary key,
+barrio varchar(15),
+capacidad int
+);
+insert into estaciones values
+(1,"Gracia",10),
+(2,"Carmelo",20),
+(3,"Example",15),
+(4,"Maresme",8);
 
 
 create table bicicletas(
 id_bici int primary key,
 estado_bici varchar(10),
-activa_bici bool
+activa_bici boolean,
+parking int,
+constraint fk10 foreign key (parking)
+references estaciones(id_est)
+on delete restrict on update cascade
+
 );
 insert into bicicletas values
-(1,"Active",true),
-(2,"No_Active",false),
-(3,"Active",true),
-(4,"No_Active",true),
-(5,"Active",true),
-(6,"Active",true),
-(7,"Active",true),
-(8,"Active",true),
-(9,"No_Active",false),
-(10,"Active",true),
-(11,"Active",true);
+(1,"Active",true,1),
+(2,"No_Active",false,2),
+(3,"Active",true,1),
+(4,"No_Active",true,4),
+(5,"Active",true,2),
+(6,"Active",true,3),
+(7,"Active",true,3),
+(8,"Active",true,2),
+(9,"No_Active",false,4),
+(10,"Active",true,3),
+(11,"Active",true,4);
 
 create table alquileres(
 id_alq int primary key auto_increment,
@@ -56,16 +80,24 @@ select @f_alta;
 select @f_modif;
 SELECT TIMESTAMPDIFF(MINUTE, @f_alta, @f_modif) AS minutos_transcurridos;
 
-insert into alquileres values
-(null,"2020-02-05 19:19:54","2020-02-05 19:40:44",1,"53118274P");
+insert into alquileres values (null,"2020-02-05 19:19:54","2020-02-05 19:40:44",1,"53118274P");
+insert into alquileres values (null,"2020-02-05 10:20:54","2020-02-05 10:30:54",2,"93318174B");
+insert into alquileres values (null,"2020-02-05 11:09:14","2020-02-05 09:10:09",3,"13315174E");
+insert into alquileres values (null,"2020-02-05 19:19:54","2020-02-05 19:55:15",4,"53315174C");
+insert into alquileres values (null,"2020-02-05 19:00:00","2020-02-05 19:10:20",5,"73315174Y");
+insert into alquileres values (null,"2020-02-05 14:30:00","2020-02-05 15:00:17",6,"84445174W");
+insert into alquileres values (null,"2020-02-05 10:30:30","2020-02-05 10:45:00",8,"52215174T");
+insert into alquileres values (null,"2020-02-05 20:20:00","2020-02-05 21:30:55",9,"21115174V");
+insert into alquileres values (null,"2020-02-05 19:00:54","2020-02-05 19:40:44",10,"63315174C");
+
 
 
 
 
 
 create table facturas(
-id_fact int primary key auto_increment,
-fecha_fact datetime default current_timestamp,
+id_fact int primary key auto_increment,       
+fecha_fact datetime default current_timestamp,     						
 usuario varchar(9),
 constraint fk3 foreign key (usuario)
 references usuarios(dni)
@@ -76,47 +108,40 @@ insert into facturas values
 (null,default,"93318174B");
 
 
-create table estaciones(
-id_est int primary key,
-barrio varchar(15),
-capacidad int
-);
-insert into estaciones values
-(1,"Gracia",10),
-(2,"Carmelo",10),
-(3,"Example",10),
-(4,"Maresme",10);
 
 
--- CONSULTAS
+
+-- CONSULTAS 
 
 -- #001 Obtener todos los usuarios.
 		select * from usuarios;
-
--- #002 Obtener todos los usuarios.
+-- #002 Obtener todos los usuarios. 
 		select * from bicicletas;
-
--- #003 Obtener todos los alquileres.
+-- #003 Obtener todos los alquileres. 
 		select * from alquileres;
-
--- #003 Obtener todos los alquileres.
+-- #003 Obtener todos los alquileres. 
 		select * from facturas;
+-- #004	Obtener las Bicicletas que estan Activas.
+		select * from bicicletas where estado_bici = "Active";
+-- #005 Obtener las Bicicletas que no estan en funcionamiento.
+		select * from bicicletas where estado_bici = "No_Active";
+-- #006 Obtener el número de bicicletas que hay en cada estacion.
+		select count(*) from bicicletas group by parking;
+-- #007 Activar las bicis
+		update estado_bici set estado_bici = "Active" where estado_bici = "No_Active";
+-- #008 Anyadir una nueva estacio de bicicletas
+		insert into estaciones values (5,"Sant Gervasi",10); 
+-- #009 Crear una lista de todos los DNI
+		select dni from usuarios;
+-- #010 Saber cuantas bicicletas hay activas en la parada de Gracia.
+		select id_bici from bicicletas  join estaciones on parking=id_est where  estado_bici = "Active"  and  barrio =  "Gracia" ;
+-- #011 
+		select * from customers order by    desc;
+-- #012
 
--- #004
+-- #013
 
--- #005
-
--- #006
-
--- #007
-
--- #008
-
--- #009
-
--- #010
-
--- #011
+-- #014
 
 -- #015
 
