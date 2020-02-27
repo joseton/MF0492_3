776cc2100632c08797d17043ce9596034c6ab2fa
mateu85 <mateupego85@hotmail.com>
2020-02-27 20:06:20 +0100

    20:06 27/02/2020

--- php/ejer3.php
+++ php/ejer3.php
@@ -1,7 +1,65 @@
 <?php
 // TODO Challenge 3: Añadir el código PHP que se indica en el ejercicio del Challenge 3
 
-// Confeccionar una clase Persona que tenga como atributos el nombre y la edad. Definir como métodos uno que cargue los datos personales (iniciallizar atributos) y otro que los imprima. Plantear una segunda clase Empleado que herede de la clase Persona. Añadir un atributo sueldo y los métodos de cargar el sueldo e imprimir su sueldo. Definir un objeto de la clase Persona y llamar a sus métodos. También crear un objeto de la clase Empleado y llamar a sus métodos.
+// Confeccionar una clase Persona que tenga como atributos el nombre y la edad.
+
+class Persona{
+    // atributos
+    public $nombre;
+    protected $edad;
+
+    //Definir como métodos uno que cargue los datos personales (iniciallizar atributos) y otro que los imprima.
+
+    public function inicializar($n,$e) {
+
+        $this->nombre = $n;
+        $this->edad = $e;
+
+    }
+
+    public function imprimir(){
+
+        echo "El nombre del Empleado es: ".$this->nombre."<br>";
+        echo "Su edad es de: ".$this->edad."<br>";
+
+    }
+}
+
+//Plantear una segunda clase Empleado que herede de la clase Persona. Añadir un atributo sueldo y los métodos de cargar el sueldo e imprimir su sueldo.
+
+class Empleado extends Persona{
+
+    public $sueldo;
+
+    public function cargar($s){
+
+        $this->sueldo = $s;
+
+    }
+
+
+    public function visualizar(){
+        echo "Nombre ".$this->nombre."<br>";
+        echo "Edad ".$this->edad."<br>";
+        echo "Sueldo ".$this->sueldo."<br>";
+    }
+
+}
+
+//Definir un objeto de la clase Persona y llamar a sus métodos. También crear un objeto de la clase Empleado y llamar a sus métodos.
+
+
+
+$persona1 = new Persona;
+$persona1->inicializar("Jose",28);
+$persona1->imprimir();
+
+$empleado1 = new Empleado;
+$empleado1->inicializar("JoseLuis",48);
+$empleado1->cargar(2550);
+$empleado1->visualizar();
+
+
 
 
 
--- php/ejer4.php
+++ php/ejer4.php
@@ -2,7 +2,12 @@
 // TODO Challenge 3: Añadir el código PHP que se indica en el ejercicio del Challenge 3
 
 // Confeccionar una clase Menu.
+
+
+
 // Permitir añadir como atributos el nombre del enlace y la url.
+
+
 // Mostrar el menú en forma horizontal o vertical según que método llamemos.
 
 
