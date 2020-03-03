aca17314f597c74bc03db759183c611225211f26
Armando <47246587+armandolap@users.noreply.github.com>
2020-03-03 17:11:55 +0100

    3/3/20

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
-// Bootstrap.php: Requiere del archivo de configuración con la que funcionará la aplicación
+// Bootstrap.php: Requiere del archivo de configuración inicial con la que funcionará la aplicación
 require dirname(__DIR__) . '/Core/Bootstrap.php';
 // dirname(__DIR__): Devuelve la ruta de la carpeta raiz del proyecto
 // CHECK
 // echo dirname(__DIR__);
+
+// La clase ReflectionClass devuelve la información sobre una clase 'FrontController'
+$reflector = new ReflectionClass(APP);
+$app = $reflector->newInstance();
+$app->run();
