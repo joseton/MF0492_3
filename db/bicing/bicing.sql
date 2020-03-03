2dba62a54502412c51970197d529838471af977c
Armando <47246587+armandolap@users.noreply.github.com>
2020-03-03 13:43:28 +0100

    3/3/20

--- db/bicing/bicing.sql
+++
@@ -1,4 +0,0 @@
--- TODO Challenge 1: Añadir el código SQL que se indica en la descripción del Challenge 1
-
-
--- Final TODO Challenge 1
--- db/bicing/bicing_prog.sql
+++
@@ -1,4 +0,0 @@
--- TODO Challenge 2: Añadir el código SQL que se indica en la descripción del Challenge 2
-
-
--- Final TODO Challenge 2
