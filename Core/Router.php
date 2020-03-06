fd0926973a2069ba52154ed6928267cbfd721b03
mateu85 <mateupego85@hotmail.com>
2020-03-06 16:47:45 +0100

    16:47 06/03/2020

--- Core/Router.php
+++ Core/Router.php
@@ -53,8 +53,37 @@ class Router{
     }
     // TODO Challenge 4: Añadir el código PHP que se indica en el ejercicio del Challenge 4
 
+    public function getAction($url){
+        // (array)$this->routes : convierte el atributo en array
+        $routesArray = (array)$this->routes;
+        // CHECK
+        // var_dump($routesArray);
 
+        // elimina toda el texto de la url precedido de '/'
+        $url = ltrim($url, '/');
+        // Nos quedamos con el texto después de '/'
+        $url = substr($url, strpos($url, '/') + 1);
+
+        return $routesArray[$url]->action;
 
     // Final TODO Challenge 4
 
+    }
+
 }
+
+// #Challenge4: Desarrollar el método 'getAction' en el 'Router' que d…
+
+
+
+/*
+- Este método se llama desde el 'FrontController', recoge la URL del request  y comprueba la acción (método) asignada en 'routes.json.
+- Si no existe acción (null) en 'routes.json', carga por defecto el método 'index' del controlador 'HomeController'.
+
+- 'HomeController':
+    - Añadir un método 'challenge4' que contiene un 'echo' con el siguiente texto: "Soy el Home Controller con acción (challenge4)"
+
+- 'routes.json':
+    - Añadir la ruta "home/challenge4". que cargará el controlador 'HomeController' y la acción 'challenge4'
+
+- COMPROBAR con 'CHECK' la carga de los 'echo' en el 'FrontController'*/
